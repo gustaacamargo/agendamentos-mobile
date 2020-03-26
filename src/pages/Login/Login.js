7193b551f8387c8d7bb3d428c08a5378eed7cdff
gustaacamargo <camargo25.gustavo@gmail.com>
2020-03-26 17:09:39 -0300

    Finaliza tela de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,28 +1,83 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableHighlight, Image, KeyboardAvoidingView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, KeyboardAvoidingView, ActivityIndicator, AsyncStorage } from 'react-native';
 import DismissKeyboard from '../../utils/dismissKeyboard';
 import Logo from '../../../assets/logo.png';
+import api from '../../services/api';
 
 function Login( {navigation} ) {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    async function onLogin() {
+        if (!username || !password) {
+            setError("Preencha todos os campos para continuar!");
+        } else {
+            try {
+                setIsLoading(true);
+                
+                const response = await api.post("/sessions", {                     
+                        username, 
+                        password                   
+                });
+
+                await AsyncStorage.setItem('@AirBnbApp:token', response.data.token);
+
+            } catch (err) {
+                setError("Nome de usuário ou senha incorreta.");
+                console.log(err);
+                
+            }
+            setIsLoading(false);
+        }
+    }
 
     return(
         <DismissKeyboard>
             <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-
-            {/* <View style={styles.container}> */}
                 <Image style={styles.logo} source={Logo} resizeMode="contain"/>
-                <TextInput style={styles.input} placeholder="Nome de usuário"></TextInput>
-                <TextInput style={styles.input} placeholder="Senha"></TextInput>
-                <TouchableHighlight style={styles.button}>
+                {(error) && <Text style={styles.errorText}>{error}</Text>}
+                <TextInput 
+                    autoCapitalize="none" 
+                    autoCorrect={false} 
+                    style={styles.input} 
+                    placeholder="Nome de usuário"
+                    value={username}
+                    onChangeText={setUsername}
+                ></TextInput>
+                <TextInput 
+                    autoCapitalize="none" 
+                    autoCorrect={false} 
+                    secureTextEntry 
+                    style={styles.input} 
+                    placeholder="Senha"
+                    value={password}
+                    onChangeText={setPassword}
+                ></TextInput>
+                <TouchableOpacity onPress={onLogin} style={styles.button}>
                     <Text style={styles.buttonText}>Login</Text>
-                </TouchableHighlight>
-            {/* </View> */}
+                </TouchableOpacity>
+
+                { isLoading && (
+                <View style={styles.loading}>
+                    <ActivityIndicator size="large" color="#FFF" />    
+                </View>)}
+
             </KeyboardAvoidingView>
         </DismissKeyboard>
     );
 }
 
 const styles = StyleSheet.create({
+    loading: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0,0,0,0.8)',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
     container: {
         flex: 1,
         alignItems: 'center',
@@ -54,6 +109,12 @@ const styles = StyleSheet.create({
     buttonText: {
         color: '#FFF',
         fontWeight: 'bold',
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    errorText: {
+        marginBottom: 10,
+        color: '#FF0000',
         fontSize: 16,
         textAlign: 'center',
     }
