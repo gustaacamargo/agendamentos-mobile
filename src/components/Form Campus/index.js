fb43456b5e9ad37e0e13145dd4cb87defba75ca5
Gustavo Camargo <camargo25.gustavo@gmail.com>
2021-05-17 22:45:16 -0300

    Adiciona lista de cidades via IBGE

--- src/components/Form Campus/index.js
+++ src/components/Form Campus/index.js
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, ActivityIndicator, Alert } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import axios from "axios"
+import Select2 from "react-native-select-two";
 
 function FormCampus({ onSubmit, campus, navigation }) {
 
     const [city, setCity] = useState('');
     const [adress, setAdress] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [cities, setCities] = useState([])
+
+    useEffect(() => {
+        getCities()
+    }, [])
 
     useEffect(() => {
         if(campus){
@@ -14,6 +21,26 @@ function FormCampus({ onSubmit, campus, navigation }) {
             setAdress(campus.adress);
         }
     }, []);
+
+    function getCities() {
+        axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/41/municipios?orderBy=nome')
+        .then(res => {
+            const array = []
+            res.data.map(city => {
+                if(campus) {
+                    if(city.nome == campus.city) {
+                        setCity(city.nome)
+                        city.checked = true
+                    }
+                }
+                city.name = city.nome
+                city.id = city.nome
+                array.push(city)
+            })
+            setCities(array)
+        })
+        .catch(err => MySwal.fire('Oops', 'Erro ao recuperar lista de cidades', 'error'))
+    }
 
     function save() {   
         if(!city) { Alert.alert('Campo obrigatório', 'O campo cidade deve ser preenchido'); return }
@@ -46,12 +73,20 @@ function FormCampus({ onSubmit, campus, navigation }) {
             <View style={styles.row}>
                 <View style={styles.card}>
                     <Text style={styles.titleText}>Cidade *</Text>
-                    <TextInput 
-                        keyboardType="default" 
-                        style={styles.input} 
-                        placeholder="Cidade"
-                        value={city}
-                        onChangeText={setCity}
+                    <Select2
+                        isSelectSingle
+                        style={styles.input}
+                        colorTheme="blue"
+                        popupTitle="Selecione uma cidade"
+                        searchPlaceHolderText="Pesquisar"
+                        title="Cidade"
+                        data={cities}
+                        onSelect={data => {
+                            setCity(data[0]);                                           
+                        }}
+                        cancelButtonText="Cancelar"
+                        selectButtonText="Selecionar"
+                        listEmptyTitle="Não há nada aqui"
                     />
                 </View>
             </View>
